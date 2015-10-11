c9cfc4d4a24801099b17f903b23a237b068bcd5a
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-10-11 23:17:17 +0300

    Fix place displaying

--- src/js/components/Schedule/Place.jsx
+++ src/js/components/Schedule/Place.jsx
@@ -8,7 +8,7 @@ var Place = React.createClass({
                 {this.props.data.map(function(place, i) {
                     return (
                         <div key={i}>
-                            {place.building && <span>{place.building.name}, {place.building.address}</span> }
+                            {place.building && <span>{place.building.name}, {place.building.address} </span> }
                             {place.name && <span>ауд. {place.name}</span> }
                         </div>
                     )
