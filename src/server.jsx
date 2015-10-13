26c09adb6f7ede9d05e3308498e7eb763141e989
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-10-13 23:00:44 +0300

    Make express server works

--- src/server.jsx
+++ src/server.jsx
@@ -3,6 +3,9 @@ var app = express();
 var fs = require('fs');
 var html = fs.readFileSync('./index.html');
 
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
+
 app.use(function(req, res) {
     res.end(html);
 });
