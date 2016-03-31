4d5644cb8bd028fc15478296a21eec7f93afca7c
Scorik Egor <shirofaii@gmail.com>
2016-03-31 20:07:53 +0300

    filter for teachers
    change filter to more complex:
    if only one word in filter then make filter in this particular order: 1)
    by family 2) by name
    many words uses whole name for filter

--- src/js/components/TeacherListFilter.jsx
+++ src/js/components/TeacherListFilter.jsx
@@ -20,6 +20,30 @@ var TeacherListFilter = React.createClass({
         this.setState({filter: newFilter});
     },
 
+    filterTeachers: function(filter) {
+        var words = filter.match(/\S+/ig)
+        var teachers = this.props.teachers
+        if (words.length === 0) return teachers;
+
+        // NB: `first_name` `middle_name` `last_name` fields in teacher json object
+        // first_name = actually surname
+        // middle_name = actually first name
+        // last_name = actually middle name
+        // very confusing
+
+        // if only one word in filter then make filter in this particular order: 1) by family 2) by name
+        if (words.length === 1) {
+            var filteredByFamily = teachers.filter(teacher => teacher.first_name.toLowerCase().indexOf(words[0].toLowerCase()) == 0)
+            var filteredByName = teachers.filter(teacher => teacher.middle_name.toLowerCase().indexOf(words[0].toLowerCase()) == 0)
+
+            return _.union(filteredByFamily, filteredByName)
+        }
+
+        // many words uses whole name for filter
+        return teachers.filter(function(teacher) {
+            return _.every(words, word => teacher.full_name.toLowerCase().search(word.toLowerCase()) != -1)
+        });
+    },
 
     render: function() {
         let teachers = this.props.teachers;
@@ -41,11 +65,7 @@ var TeacherListFilter = React.createClass({
         }
 
         if (this.state.filter.length > 0) {
-            let filter = this.state.filter.toLowerCase()
-            teachers = teachers.filter(teacher =>
-                // its very strange indeed, but first_name is actualy surname
-                teacher.first_name.toLowerCase().indexOf(filter) == 0
-            )
+            teachers = this.filterTeachers(this.state.filter)
         }
 
         return (
